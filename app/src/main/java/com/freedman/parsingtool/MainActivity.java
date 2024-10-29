892d36338754eb641741552ee270fb46643a5297
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-29 13:04:52 -0400

    Refactored Import Button

--- app/src/main/java/com/freedman/parsingtool/MainActivity.java
+++ app/src/main/java/com/freedman/parsingtool/MainActivity.java
@@ -1,24 +1,24 @@
 package com.freedman.parsingtool;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button importData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        setViews();
+
+    }
+
+    private void setViews() {
+        importData = findViewById(R.id.import_data);
     }
 }
