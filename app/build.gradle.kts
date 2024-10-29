50c1a24226048d219b3433ffda3729dde449fef9
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-29 19:57:00 -0400

    updated min sdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.freedman.parsingtool"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
