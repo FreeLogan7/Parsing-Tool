22eef2ca23056881ad561816c8588318e34e123a
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-30 19:32:43 -0400

    Implemented JsonFileReader

---
+++ app/src/main/java/com/freedman/parsingtool/filereader/JsonFileReader.java
@@ -0,0 +1,22 @@
+package com.freedman.parsingtool.filereader;
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public class JsonFileReader implements FileReader {
+
+    public List<Map<String, Object>> read(InputStream inputStream) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+
+        return mapper.readValue(inputStream, new TypeReference<List<Map<String, Object>>>(){});
+    }
+}
+
