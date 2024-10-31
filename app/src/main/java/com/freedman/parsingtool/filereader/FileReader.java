a55b5d78cb2601669200d44aa2076453a0bad255
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-30 20:46:55 -0400

    Implemented Reader Interface

--- app/src/main/java/com/freedman/parsingtool/filereader/FileReader.java
+++ app/src/main/java/com/freedman/parsingtool/filereader/FileReader.java
@@ -1,2 +1,10 @@
-package com.freedman.parsingtool;public interface FileReader {
+package com.freedman.parsingtool.filereader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+public interface FileReader {
+    List<Map<String, Object>> read(InputStream inputStream) throws IOException;
 }
