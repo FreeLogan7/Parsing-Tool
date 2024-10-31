4ee7fb50b5a1584be28896524a2c23658488e4dc
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-31 17:06:19 -0400

    Updated FileWriter Name due to name conflict

--- app/src/main/java/com/freedman/parsingtool/filereader/FileWriterInterface.java
+++ app/src/main/java/com/freedman/parsingtool/filereader/FileWriterInterface.java
@@ -3,10 +3,9 @@ package com.freedman.parsingtool.filereader;
 import android.content.Context;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
-public interface FileWriter {
+public interface FileWriterInterface {
     void write(Context context, List<Map<String, Object>> data) throws IOException;
 }
