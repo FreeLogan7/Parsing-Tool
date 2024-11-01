0f7b2d09bd104ac19781cc170426c9374957545d
FreeLogan7 <163227178+FreeLogan7@users.noreply.github.com>
2024-10-31 21:39:42 -0400

    Implemented new buttons

--- app/src/main/java/com/freedman/parsingtool/MainActivity.java
+++ app/src/main/java/com/freedman/parsingtool/MainActivity.java
@@ -12,6 +12,7 @@ import android.system.Os;
 import android.system.OsConstants;
 import android.system.StructStat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
@@ -34,8 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     private Button importData;
     ActivityResultLauncher<String> mGetContent;
-
     private FileConverter converter = new FileConverter();
+    CheckBox saveFile;
+    CheckBox saveDatabase;
+    CheckBox convertToJson;
+    CheckBox convertToCsv;
 
 
     @Override
@@ -43,39 +47,90 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         setViews();
         setupImport();
         importButtonClicked();
+        saveButtons();
+        convertButtons();
+
 
     }
 
+    private void convertButtons() {
+        convertToJson.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                convertToCsv.setChecked(false);
+            }
+        });
+        convertToCsv.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                convertToJson.setChecked(false);
+            }
+        });
+    }
+
+    private void saveButtons() {
+        saveFile.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                saveDatabase.setChecked(false);
+                convertToCsv.setVisibility(View.VISIBLE);
+                convertToJson.setVisibility(View.VISIBLE);
+            } else if (!saveFile.isChecked()) {
+                convertToCsv.setVisibility(View.INVISIBLE);
+                convertToJson.setVisibility(View.INVISIBLE);
+                clearConvertCheckbox();
+            }
+        });
+
+        saveDatabase.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                saveFile.setChecked(false);
+                convertToCsv.setVisibility(View.INVISIBLE);
+                convertToJson.setVisibility(View.INVISIBLE);
+                clearConvertCheckbox();
+            }
+
+        });
+    }
+
+    private void clearConvertCheckbox() {
+        convertToJson.setChecked(false);
+        convertToCsv.setChecked(false);
+    }
+
+
     private void setViews() {
         importData = findViewById(R.id.import_data);
+        saveFile = findViewById(R.id.checkbox_file);
+        saveDatabase = findViewById(R.id.checkbox_database);
+        convertToJson = findViewById(R.id.checkbox_json);
+        convertToCsv = findViewById(R.id.checkbox_csv);
     }
 
     //On Import Button Click SELECT any file!
     private void importButtonClicked() {
         importData.setOnClickListener(v -> {
-            CheckBox saveSelection = findViewById(R.id.checkbox_database);
             mGetContent.launch("*/*");
         });
     }
 
+    //Consider Discussing Security validation for uri
     private void setupImport() {
         mGetContent = registerForActivityResult(new GetContent(), uri -> {
             if (uri == null) throw new IllegalArgumentException("uri is Null!");
-            //Display if file selected exists with name
             Toast.makeText(this, "Selected file: " + uri.toString(), Toast.LENGTH_SHORT).show();
 
             createContentResolver(uri);
         });
     }
 
-
     private void createContentResolver(Uri uri) {
         ContentResolver resolver = getContentResolver();
         try {
-            converter.convert(uri, resolver);
+            //In Future Find solution to avoid sending 'this'
+
+            converter.convert(uri, resolver, this, saveFile, saveDatabase, convertToJson, convertToCsv);
         } catch (IllegalArgumentException | IOException | CsvValidationException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             Log.e("TOASTY-ERROR", e.getMessage(), e);
